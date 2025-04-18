5608a6158623f53f95251131bb0535b29ed5811f
Mahbub-limon <mahbublimon1127@gmail.com>
2025-04-19 02:43:35 +0600

    Add project files.

---
+++ deepSeekWeb01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="deepSeekWeb01.MvcApplication" Language="C#" %>
---
+++ deepSeekWeb01/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace deepSeekWeb01.Models
+{
+    public class Product
+    {
+        public int Id { get; set; } //Primary key
+
+
+        [Required(ErrorMessage = "Name is required")] //validation
+        public string Name { get; set; }
+
+
+        [Range(1,100,ErrorMessage ="Price must be $1-$100")]
+        public decimal price {  get; set; }
+     }
+}
